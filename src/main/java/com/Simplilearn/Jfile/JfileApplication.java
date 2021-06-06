cc6e805c4b16ee26f1dc2eefaf04e070f0c34695
kanlawr2005 <kanlawr_2005@yahoo.co.in>
2021-06-06 11:50:03 +0000

    first commit

---
+++ src/main/java/com/Simplilearn/Jfile/JfileApplication.java
@@ -0,0 +1,13 @@
+package com.Simplilearn.Jfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Simplilearn/Jfile/JfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Simplilearn.Jfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
